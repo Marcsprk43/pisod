9475fbe2c6fed77586306043207e0573ecf4245e
gerrievanzyl <43932356+Marcsprk43@users.noreply.github.com>
2022-07-08 12:47:34 -0400

    new auto cal

--- mpu6050/MPU6050_calibrate_gvz.py
+++ mpu6050/MPU6050_calibrate_gvz.py
@@ -66,9 +66,9 @@ gyro_offset[2] = mpu.get_z_gyro_offset_TC()
 print('Gyro offsets: {}'.format(gyro_offset))
 
 # supply your own gyro offsets here, scaled for min sensitivity
-mpu.set_x_gyro_offset_TC(int(756/4))
-mpu.set_y_gyro_offset_TC(int(108/4))
-mpu.set_z_gyro_offset_TC(int(28/4))
+#mpu.set_x_gyro_offset_TC(int(756/4))
+#mpu.set_y_gyro_offset_TC(int(108/4))
+#mpu.set_z_gyro_offset_TC(int(28/4))
 
 gyro_offset[0] = mpu.get_x_gyro_offset_TC()
 gyro_offset[1] = mpu.get_y_gyro_offset_TC()

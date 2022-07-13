d2ff199d505cfbbfba35cc300e26691949dc68e2
Marcsprk43 <marcfvanzyl@gmail.com>
2022-07-13 16:20:00 -0400

    Added opencv_speed_test.py

---
+++ opencv_speed_test.py
@@ -0,0 +1,74 @@
+# import the necessary packages
+import datetime
+class FPS:
+	def __init__(self):
+		# store the start time, end time, and total number of frames
+		# that were examined between the start and end intervals
+		self._start = None
+		self._end = None
+		self._numFrames = 0
+	def start(self):
+		# start the timer
+		self._start = datetime.datetime.now()
+		return self
+	def stop(self):
+		# stop the timer
+		self._end = datetime.datetime.now()
+	def update(self):
+		# increment the total number of frames examined during the
+		# start and end intervals
+		self._numFrames += 1
+	def elapsed(self):
+		# return the total number of seconds between the start and
+		# end interval
+		return (self._end - self._start).total_seconds()
+	def fps(self):
+		# compute the (approximate) frames per second
+		return self._numFrames / self.elapsed()
+
+
+import  cv2
+import numpy as np
+import time
+
+
+
+fps = FPS()
+
+# Create a VideoCapture object and read from input file
+# If the input is the camera, pass 0 instead of the video file name
+cap = cv2.VideoCapture(0)
+
+# Check if camera opened successfully
+if (cap.isOpened()== False): 
+  print("Error opening video stream or file")
+
+fps.start()
+
+# Read until video is completed
+while(cap.isOpened()):
+  # Capture frame-by-frame
+  ret, frame = cap.read()
+  if ret == True:
+
+    # Display the resulting frame
+    cv2.imshow('Frame',frame)
+
+    fps.update()
+
+    # Press Q on keyboard to  exit
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+      break
+
+  # Break the loop
+  else: 
+    break
+
+fps.stop()
+
+print('FPS = {}'.format(fps.fps()))
+# When everything done, release the video capture object
+cap.release()
+
+# Closes all the frames
+cv2.destroyAllWindows()    

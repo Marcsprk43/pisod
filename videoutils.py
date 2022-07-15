034065aeb4f4dc61f2c15114edc7ab406fbecec7
gerrievanzyl <43932356+Marcsprk43@users.noreply.github.com>
2022-07-15 13:41:01 -0400

    back to opencv

--- videoutils.py
+++ videoutils.py
@@ -225,7 +225,7 @@ class FPS:
 font = cv2.FONT_HERSHEY_SIMPLEX
 
 def draw_altitude(frame):
-    cv2.putText(frame, 'A:{:2.1f}'.format(mv.data['Altitude']), (650, 25), font, 1, (0, 0, 255), 2, cv2.LINE_AA)
+    cv2.putText(frame, 'Alt:{:2.1f}'.format(mv.data['Altitude']), (650, 25), font, 1, (0, 0, 255), 2, cv2.LINE_AA)
 
 def draw_lat_lon(frame):
     cv2.putText(frame, 'Lat:{:.6f}'.format(mv.data['Lat']), (5, 25), font, 1, (255,0, 0), 2, cv2.LINE_AA)
@@ -239,7 +239,7 @@ def draw_cross_hairs(frame):
     cv2.line(frame, (350,300), (450,300), (0, 0, 255), 3) 
 
 def draw_flight_mode(frame):
-    cv2.putText(frame, 'FM:{}'.format(mv.data['FlightMode']), (700, 590), font, 1, (0,255, 0), 2, cv2.LINE_AA)
+    cv2.putText(frame, 'FM:{}'.format(mv.data['FlightMode']), (600, 590), font, 1, (0,255, 0), 2, cv2.LINE_AA)
 
 
 def apply_osd(frame, osd):

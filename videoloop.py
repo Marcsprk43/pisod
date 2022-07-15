a68e5f564203ac00981fe451cc6abf90038337ed
gerrievanzyl <43932356+Marcsprk43@users.noreply.github.com>
2022-07-15 11:31:55 -0400

    simpleaccel

--- videoloop.py
+++ videoloop.py
@@ -4,8 +4,50 @@ import cv2
 import numpy as np
 import videoutils as vu
 
+####################################################################
+# Set up the accelerometer for roll/pitch calcs
+####################################################################
+from MPU6050 import MPU6050
+from math import sqrt, asin, atan, degrees
+
+i2c_bus = 1
+device_address = 0x68
+# The offsets are different for each device and should be changed
+# accordingly using a calibration procedure
+x_accel_offset = -0
+y_accel_offset = -0
+z_accel_offset = 0
+
+# Gyro offsets: [181, 32, 6] 2022-07-13
+x_gyro_offset = 181
+y_gyro_offset = 32
+z_gyro_offset = 6
+
+enable_debug_output = True
+
+mpu = MPU6050(i2c_bus, device_address, x_accel_offset, y_accel_offset,
+              z_accel_offset, x_gyro_offset, y_gyro_offset, z_gyro_offset,
+              sample_rate_divider=1, dlpf=0x04,
+              a_debug=enable_debug_output)
+
+# function to get roll and pitch (in radians)
+def get_roll_pitch():
+    accel = mpu.get_acceleration()    
+    pitch = asin(accel[0]/sqrt(accel[0]*accel[0] + accel[1]*accel[1]+ accel[2]*accel[2] ))
+    roll = atan(accel[1]/accel[2])
+    return roll, pitch
+
+
+####################################################################
 # this is simple class to calculate frames per second
+####################################################################
 fps = vu.FPS()
+
+
+
+####################################################################
+# Setup the camera
+####################################################################
 
 sensor = vu.sensor_IMX219 # select the PiCamera V2.1 sensor
 
@@ -17,8 +59,9 @@ camera = vu.configure_camera(sensor, lens_f=2.1, image_mode=1, frame_rate=1)
 # If the input is the camera, pass 0 instead of the video file name
 cap = cv2.VideoCapture(0)
 
-cap.set(3, 1250)  # Set horizontal resolution
-cap.set(4, 720)  # Set vertical resolution
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1250)  # Set horizontal resolution
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)  # Set vertical resolution
+cap.set(cv2.CAP_PROP_FPS, 15)
 
 # Check if camera opened successfully
 if (cap.isOpened()== False): 
@@ -28,12 +71,18 @@ fps.start()
 
 
 while(1):
-    
+
   # Capture frame-by-frame
   ret, frame = cap.read()
   if ret == True:
+    # get the instantaneous roll/pitch
+    roll, pitch = get_roll_pitch()
 
-    shift_frame = vu.image_tranlate(frame, 200,200)
+    # calc the pixel shift
+    dph, dpw = get_pixel_shift(roll, pitch, camera)
+
+
+    shift_frame = vu.image_tranlate(frame, dph, dpw)
     
     # Display the resulting frame
     cv2.imshow('Frame',shift_frame)
@@ -41,7 +90,7 @@ while(1):
     fps.update()
 
     # Press Q on keyboard to  exit
-    if cv2.waitKey(25) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord('q'):
       break
 
   # Break the loop

dddf781faa6363a13b9b234373c4479a01375d05
gerrievanzyl <43932356+Marcsprk43@users.noreply.github.com>
2022-07-15 14:27:04 -0400

    back to opencv

--- videoloop.py
+++ videoloop.py
@@ -67,7 +67,7 @@ cap = cv2.VideoCapture(0)
 
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 800)  # Set horizontal resolution
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 600)  # Set vertical resolution
-cap.set(cv2.CAP_PROP_FPS, 10)
+cap.set(cv2.CAP_PROP_FPS, 30)
 
 # Check if camera opened successfully
 if (cap.isOpened()== False): 

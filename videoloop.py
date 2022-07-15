2ddc513996378d18b361007696b06a28c32191fd
gerrievanzyl <43932356+Marcsprk43@users.noreply.github.com>
2022-07-15 13:54:03 -0400

    back to opencv

--- videoloop.py
+++ videoloop.py
@@ -112,7 +112,7 @@ while(1):
 
     elif mode == ST_STABILIZE_VIDEO:
       # calc the pixel shift
-      dph, dpw = vu.get_pixel_shift(roll, pitch, camera, factor=.8)
+      dph, dpw = vu.get_pixel_shift(roll, pitch, camera, factor=.75)
 
       frame = vu.image_tranlate(frame, dph, dpw)
 

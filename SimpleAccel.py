97cd0eed3471ea98c14fda571a027d7b9f0aae12
gerrievanzyl <43932356+Marcsprk43@users.noreply.github.com>
2022-07-15 10:06:28 -0400

    simpleaccel.py

---
+++ SimpleAccel.py
@@ -0,0 +1,103 @@
+#Connections
+#MPU6050 - Raspberry pi
+#VCC - 5V  (2 or 4 Board)
+#GND - GND (6 - Board)
+#SCL - SCL (5 - Board)
+#SDA - SDA (3 - Board)
+
+
+import smbus			#import SMBus module of I2C
+import time
+import math
+
+
+RestrictPitch = True	#Comment out to restrict roll to ±90deg instead - please read: http://www.freescale.com/files/sensors/doc/app_note/AN3461.pdf
+radToDeg = 57.2957786
+
+#some MPU6050 Registers and their Address
+PWR_MGMT_1   = 0x6B
+SMPLRT_DIV   = 0x19
+CONFIG       = 0x1A
+GYRO_CONFIG  = 0x1B
+INT_ENABLE   = 0x38
+ACCEL_XOUT_H = 0x3B
+ACCEL_YOUT_H = 0x3D
+ACCEL_ZOUT_H = 0x3F
+GYRO_XOUT_H  = 0x43
+GYRO_YOUT_H  = 0x45
+GYRO_ZOUT_H  = 0x47
+
+
+#Read the gyro and acceleromater values from MPU6050
+def MPU_Init():
+	#write to sample rate register
+	bus.write_byte_data(DeviceAddress, SMPLRT_DIV, 7)
+
+	#Write to power management register
+	bus.write_byte_data(DeviceAddress, PWR_MGMT_1, 1)
+
+	#Write to Configuration register
+	#Setting DLPF (last three bit of 0X1A to 6 i.e '110' It removes the noise due to vibration.) https://ulrichbuschbaum.wordpress.com/2015/01/18/using-the-mpu6050s-dlpf/
+	bus.write_byte_data(DeviceAddress, CONFIG, int('0000110',2))
+
+	#Write to Gyro configuration register
+	bus.write_byte_data(DeviceAddress, GYRO_CONFIG, 24)
+
+	#Write to interrupt enable register
+	bus.write_byte_data(DeviceAddress, INT_ENABLE, 1)
+
+
+def read_raw_data(addr):
+	#Accelero and Gyro value are 16-bit
+        high = bus.read_byte_data(DeviceAddress, addr)
+        low = bus.read_byte_data(DeviceAddress, addr+1)
+
+        #concatenate higher and lower value
+        value = ((high << 8) | low)
+
+        #to get signed value from mpu6050
+        if(value > 32768):
+                value = value - 65536
+        return value
+
+
+bus = smbus.SMBus(1) 	# or bus = smbus.SMBus(0) for older version boards
+DeviceAddress = 0x68   # MPU6050 device address
+
+MPU_Init()
+
+time.sleep(1)
+#Read Accelerometer raw value
+accX = read_raw_data(ACCEL_XOUT_H)
+accY = read_raw_data(ACCEL_YOUT_H)
+accZ = read_raw_data(ACCEL_ZOUT_H)
+
+print(accX,accY,accZ)
+print(math.sqrt((accY**2)+(accZ**2)+(accX**2)))
+
+roll = math.atan(accY/math.sqrt((accX**2)+(accZ**2))) * radToDeg
+pitch = math.atan2(-accX,accZ) * radToDeg
+# print(roll)
+
+
+timer = time.time()
+flag = 0
+while True:
+
+    try:
+        #Read Accelerometer raw value
+        accX = read_raw_data(ACCEL_XOUT_H)
+        accY = read_raw_data(ACCEL_YOUT_H)
+        accZ = read_raw_data(ACCEL_ZOUT_H)
+
+        #Read Gyroscope raw value
+        gyroX = read_raw_data(GYRO_XOUT_H)
+        gyroY = read_raw_data(GYRO_YOUT_H)
+        gyroZ = read_raw_data(GYRO_ZOUT_H)
+
+        print("X: " + str(accX)+"   Y: " + str(accY)+"    Z: "+str(accZ))
+        #print(str(roll)+"  "+str(gyroXAngle)+"  "+str(compAngleX)+"  "+str(kalAngleX)+"  "+str(pitch)+"  "+str(gyroYAngle)+"  "+str(compAngleY)+"  "+str(kalAngleY))
+        time.sleep(0.005)
+
+    except Exception as exc:
+        flag += 1

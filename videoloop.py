d01f7250176b18cd584f78fba8407dfb9e2c7482
gerrievanzyl <43932356+Marcsprk43@users.noreply.github.com>
2022-07-17 10:33:08 -0400

    migrated to imutils threaded capture 800x600

--- videoloop.py
+++ videoloop.py
@@ -79,6 +79,10 @@ camera = vu.configure_camera(sensor, lens_f=2.1, image_mode=5, frame_rate=30)
 # initialize the video stream and allow the cammera sensor to warmup
 vs = VideoStream(usePicamera=True, resolution=(800,600), framerate=40).start()
 
+frame = vs.read()
+
+print('frame size:', frame.shape)
+
 
 """# Create a VideoCapture object and read from input file
 # If the input is the camera, pass 0 instead of the video file name

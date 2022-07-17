3f6d5ff5e827f048fdaf602cb63dde94bf098745
gerrievanzyl <43932356+Marcsprk43@users.noreply.github.com>
2022-07-17 10:20:00 -0400

    migrated to imutils threaded capture

--- videoloop.py
+++ videoloop.py
@@ -4,6 +4,10 @@ import cv2
 import numpy as np
 import videoutils as vu
 from pymavlink import mavutil
+from imutils.video import VideoStream
+import argparse
+
+
 
 
 ####################################################################
@@ -49,6 +53,14 @@ def get_roll_pitch():
     return filtered_roll, filtered_pitch
 
 
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--picamera", type=int, default=-1,
+    help="whether or not the Raspberry Pi camera should be used")
+args = vars(ap.parse_args())
+
+
+
 ####################################################################
 # this is simple class to calculate frames per second
 ####################################################################
@@ -63,7 +75,11 @@ sensor = vu.sensor_IMX219 # select the PiCamera V2.1 sensor
 
 camera = vu.configure_camera(sensor, lens_f=2.1, image_mode=5, frame_rate=30)
 
-# Create a VideoCapture object and read from input file
+# initialize the video stream and allow the cammera sensor to warmup
+vs = VideoStream(usePiCamera=args["picamera"] > 0).start()
+
+
+"""# Create a VideoCapture object and read from input file
 # If the input is the camera, pass 0 instead of the video file name
 cap = cv2.VideoCapture(0)
 
@@ -74,7 +90,7 @@ cap.set(cv2.CAP_PROP_FPS, 30)
 # Check if camera opened successfully
 if (cap.isOpened()== False): 
   print("Error opening video stream or file")
-
+"""
 # initialize the FPS tracker
 fps.start()
 # initialize the roll pitch
@@ -107,65 +123,58 @@ mv = vu.Mavlink()
 while(1):
 
   # Capture frame-by-frame
-  ret, frame = cap.read()
-  if ret == True:
+  frame = vs.read()
+  frame = imutils.resize(frame, width=680)
 
-    ##############################################
-    # instructions that must always run 
-    ##############################################
-    # get the instantaneous roll/pitch
-    roll, pitch = get_roll_pitch()
+  ##############################################
+  # instructions that must always run 
+  ##############################################
+  # get the instantaneous roll/pitch
+  roll, pitch = get_roll_pitch()
 
-    try: 
-        the_connection.recv_match(blocking=False)
-        mv.data['Lat'] = the_connection.messages['AHRS2'].lat  # Note, you can access message fields as attributes!
-        mv.data['Lon'] = the_connection.messages['AHRS2'].lng  # Note, you can access message fields as attributes!
-        mv.data['Altitude'] = the_connection.messages['AHRS2'].altitude  # Note, you can access message fields as attributes!
-        mv.data['BattV'] = the_connection.messages['SYS_STATUS'].voltage_battery  # Note, you can access message fields as attributes!
-        mv.data['BattPercent'] = the_connection.messages['SYS_STATUS'].battery_remaining
-    except Exception as e:
-        print(e)
+  try: 
+      the_connection.recv_match(blocking=False)
+      mv.data['Lat'] = the_connection.messages['AHRS2'].lat  # Note, you can access message fields as attributes!
+      mv.data['Lon'] = the_connection.messages['AHRS2'].lng  # Note, you can access message fields as attributes!
+      mv.data['Altitude'] = the_connection.messages['AHRS2'].altitude  # Note, you can access message fields as attributes!
+      mv.data['BattV'] = the_connection.messages['SYS_STATUS'].voltage_battery  # Note, you can access message fields as attributes!
+      mv.data['BattPercent'] = the_connection.messages['SYS_STATUS'].battery_remaining
+  except Exception as e:
+      print(e)
 
-    ##############################################
-    # State machine for video modes 
-    ##############################################
+  ##############################################
+  # State machine for video modes 
+  ##############################################
 
-    if mode == ST_RAW_VIDEO:
-      # set the final frame to frame
-      vu.apply_osd(frame, osd_overlay, mv)
+  if mode == ST_RAW_VIDEO:
+    # set the final frame to frame
+    vu.apply_osd(frame, osd_overlay, mv)
 
-    elif mode == ST_STABILIZE_VIDEO:
-      # calc the pixel shift
-      dph, dpw = vu.get_pixel_shift(roll, pitch, camera, factor=.75)
+  elif mode == ST_STABILIZE_VIDEO:
+    # calc the pixel shift
+    dph, dpw = vu.get_pixel_shift(roll, pitch, camera, factor=.75)
 
-      frame = vu.image_tranlate(frame, dph, dpw)
+    frame = vu.image_tranlate(frame, dph, dpw)
 
-      # always do this last
-      vu.apply_osd(frame, osd_overlay, mv)
+    # always do this last
+    vu.apply_osd(frame, osd_overlay, mv)
 
-    elif mode == ST_FIND_TARGET:
-      # calc the pixel shift
-      dph, dpw = vu.get_pixel_shift(roll, pitch, camera)
-      # stabilize the frame
-      frame = vu.image_tranlate(frame, dph, dpw)
+  elif mode == ST_FIND_TARGET:
+    # calc the pixel shift
+    dph, dpw = vu.get_pixel_shift(roll, pitch, camera)
+    # stabilize the frame
+    frame = vu.image_tranlate(frame, dph, dpw)
 
       ### do other stuff here
 
-    else:
-      f_frame = frame.copy()
-
     
-    # Display the resulting frame
-    cv2.imshow('Frame',frame)
+  # Display the resulting frame
+  cv2.imshow('Frame',frame)
 
-    fps.update()
+  fps.update()
 
-    # Press Q on keyboard to  exit
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-      break
-
-  # Break the loop
-  else: 
+  # Press Q on keyboard to  exit
+  if cv2.waitKey(1) & 0xFF == ord('q'):
     break
 
 

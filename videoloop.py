eda93140037ecc7df94e483ae6c55cabe9660505
gerrievanzyl <43932356+Marcsprk43@users.noreply.github.com>
2022-07-17 15:08:07 -0400

    Tuning Pal

--- videoloop.py
+++ videoloop.py
@@ -79,7 +79,7 @@ sensor = vu.sensor_IMX219 # select the PiCamera V2.1 sensor
 camera = vu.configure_camera(sensor, lens_f=2.1, image_mode=5, frame_rate=32)
 
 # initialize the video stream and allow the cammera sensor to warmup
-vs = PiVideoStream(resolution=resolution), framerate=30).start()
+vs = PiVideoStream(resolution=resolution, framerate=30).start()
 
 time.sleep(1)
 

db457e97d8d7dbd1904b3a46df24acc68a17929b
gerrievanzyl <43932356+Marcsprk43@users.noreply.github.com>
2022-07-18 13:15:13 -0400

    mavlink timing

--- mavlink_1.py
+++ mavlink_1.py
@@ -1,6 +1,26 @@
 from pymavlink import mavutil
 import time
 
+def request_message_interval(master, message_id: int, frequency_hz: float):
+    """
+    Request MAVLink message in a desired frequency,
+    documentation for SET_MESSAGE_INTERVAL:
+        https://mavlink.io/en/messages/common.html#MAV_CMD_SET_MESSAGE_INTERVAL
+
+    Args:
+        message_id (int): MAVLink message ID
+        frequency_hz (float): Desired frequency in Hz
+    """
+    master.mav.command_long_send(
+        master.target_system, master.target_component,
+        mavutil.mavlink.MAV_CMD_SET_MESSAGE_INTERVAL, 0,
+        message_id, # The MAVLink message ID
+        1e6 / frequency_hz, # The interval between two messages in microseconds. Set to -1 to disable and 0 to request default rate.
+        0, 0, 0, 0, # Unused parameters
+        0, # Target address of message stream (if message has target address fields). 0: Flight-stack default (recommended), 1: address of requestor, 2: broadcast.
+    )
+
+
 # Start a connection listening on a UDP port
 the_connection = mavutil.mavlink_connection('/dev/ttyAMA0', baud=57600,
                                             dialect='ardupilotmega', autoreconnect=True)
@@ -13,7 +33,7 @@ print("Heartbeat from system (system %u component %u)" % (the_connection.target_
 the_connection.mav.system_time_send(0, 0)
 
 print('sending sys_status update rate to 30ms')
-the_connection.mav.set_message_interval_send(1,30000, 0 )
+request_message_interval(the_connection, 1, 10)
 
 count = 0 
 while ( count < 100 ):

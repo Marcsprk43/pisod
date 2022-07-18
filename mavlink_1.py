23d5a757b8bd4c46a2e13250cc52ee5ad4ca932f
gerrievanzyl <43932356+Marcsprk43@users.noreply.github.com>
2022-07-18 13:06:54 -0400

    mavlink timing

--- mavlink_1.py
+++ mavlink_1.py
@@ -10,8 +10,10 @@ the_connection = mavutil.mavlink_connection('/dev/ttyAMA0', baud=57600,
 the_connection.wait_heartbeat()
 print("Heartbeat from system (system %u component %u)" % (the_connection.target_system, the_connection.target_component))
 
+the_connection.mav.system_time_send(0, 0)
+
 print('sending sys_status update rate to 30ms')
-the_connection.mav_cmd_set_message_interval_send(1,30000, 0 )
+#the_connection.mav.sendmav_cmd_set_message_interval_send(1,30000, 0 )
 
 count = 0 
 while ( count < 100 ):

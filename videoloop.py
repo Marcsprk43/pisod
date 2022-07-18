266b27f6b4b81ddb27fee9b5c062368467779f4c
gerrievanzyl <43932356+Marcsprk43@users.noreply.github.com>
2022-07-18 15:35:57 -0400

    mavlink timing

--- videoloop.py
+++ videoloop.py
@@ -157,7 +157,8 @@ while(1):
 
   try: 
       m = the_connection.recv_msg()
-      print(m)
+      if not (m is None):
+        print(m)
       while not (m is None):  # read in all the accumulated mavlink messages
         m = the_connection.recv_msg()
         print('.', end='')

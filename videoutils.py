b7cbc6c08e14cbd89c7f3d665a1e5a1eb89b94ae
gerrievanzyl <43932356+Marcsprk43@users.noreply.github.com>
2022-07-20 14:12:45 -0400

    draw frames

--- videoutils.py
+++ videoutils.py
@@ -285,19 +285,17 @@ def rotate_vector(x,y,theta):
 def draw_capture_grid(frame, yaw,altitude):
     x = 5*360/altitude
     y = 8*288/altitude
+
     x1, y1 = rotate_vector(x, y, yaw)
     x2, y2 = rotate_vector(-x,y, yaw)
     x3, y3 = rotate_vector(-x, -y, yaw)
     x4, y4 = rotate_vector(x, -y, yaw)
 
-    cv2.line(frame, (x1+360,y1+288), (x2+360,y2+288), (0, 0, 255), 1)
-    cv2.line(frame, (x2+360,y2+288), (x3+360,y3+288), (0, 0, 255), 1)
-    cv2.line(frame, (x3+360,y3+288), (x4+360,y4+288), (0, 0, 255), 1)
-    cv2.line(frame, (x4+360,y4+288), (x1+360,y1+288), (0, 0, 255), 1)
+    cv2.line(frame, (x1+360,y1+288), (x2+360,y2+288), (0,255, 255), 1)
+    cv2.line(frame, (x2+360,y2+288), (x3+360,y3+288), (0,255, 255), 1)
+    cv2.line(frame, (x3+360,y3+288), (x4+360,y4+288), (0,255, 255), 1)
+    cv2.line(frame, (x4+360,y4+288), (x1+360,y1+288), (0,255, 255), 1)
 
-    x1, y1 = rotate_vector(0, 300, yaw)
-
-    cv2.putText(frame, 'N', (x1+360, y1+288), font, .5, (0,255, 0), 2, cv2.LINE_AA)
 
     x = x/2
     y = y/2
@@ -309,7 +307,15 @@ def draw_capture_grid(frame, yaw,altitude):
     cv2.line(frame, (x2+360,y2+288), (-x1+360,-y1+288), (0, 255, 255), 1)
     cv2.line(frame, (-x1+360,-y1+288), (-x2+360,-y2+288), (0, 255, 255), 1)
     cv2.line(frame, (-x2+360,-y2+288), (x1+360,y1+288), (0, 255, 255), 1)
+
+    x1, y1 = rotate_vector(0, y+10, yaw)
+    cv2.putText(frame, 'N+', (x1+360, y1+288), font, .5, (0,255, 255), 2, cv2.LINE_AA)
+    cv2.putText(frame, 'S-', (x1-360, y1-288), font, .5, (0,255, 255), 2, cv2.LINE_AA)
    
+    x1, y1 = rotate_vector(x+10, 0, yaw)
+    cv2.putText(frame, 'E+', (x1+360, y1+288), font, .5, (0,255, 255), 2, cv2.LINE_AA)
+    cv2.putText(frame, 'W+', (x1-360, y1-288), font, .5, (0,255, 255), 2, cv2.LINE_AA)
+  
 
 def apply_osd(frame, osd, mv):
     # possibly move this to functions:
